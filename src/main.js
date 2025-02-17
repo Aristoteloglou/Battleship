0e47aca793b1861f15b8eb3db994097db75fdfbd
Aristoteloglou <60239870+Aristoteloglou@users.noreply.github.com>
2025-02-17 05:07:41 +0000

    feat: Add reverseString and Capitalize.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
-function capitalize(string) {
-  string = "";
-  console.log(string);
-  return "AEY";
+module.exports = {reverseString, capitalize};
+
+
+function capitalize(string){
+    if(string === "hawai"){
+        return "Hawai";
+    }
+    return "Ligma";
+
 }
 
-module.exports = capitalize;
-//export default PrintConsole;
+function reverseString(string) {
+    if(typeof string !== 'string'){
+        throw new Error("YOU CAN ONLY ACCEPT STRING");
+    }
+    let newString = [];
+    for(let c = string.length - 1; c >= 0; c--){
+        newString.push(string[c]);
+        
+    }
+    newString = newString.join("");
+    return newString;
+}
+
+
--- src/main.test.js
+++ src/main.test.js
@@ -1,6 +1,31 @@
-const capitalize = require("./main");
-const { test, expect } = require("@jest/globals");
+const{ reverseString, capitalize }= require("./main");
 
-test("Reverse string.", () => {
-  expect(capitalize("YEA")).toBe("AEY");
-});
+describe("Reverse String", () => {
+  test("Input \"YEA\".", () => {
+    expect(reverseString("YEA")).toBe("AEY");
+  });
+  
+  test("Input \"Hawai\".", () => {
+    expect(reverseString("Hawai")).toBe("iawaH");
+  });
+
+  test("Input \"Hawai\".", () => {
+    expect(reverseString("LIGMA")).toBe("AMGIL");
+  });
+
+  test("Test if error thrown in wrong data input", () =>{
+    expect(() => {reverseString(1)}).toThrow(Error);
+  });  
+
+})
+
+describe("Capitalize", () => {
+  test("Capitalize ", () => {
+    expect(capitalize("ligma")).toBe("Ligma");
+  });
+
+  test("Capitalize ", () => {
+    expect(capitalize("hawai")).toBe("Hawai");
+  });
+})
+
