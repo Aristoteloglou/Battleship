d1bf17fb31b92756cc18132135b1e0fa40799a3f
Aristoteloglou <60239870+Aristoteloglou@users.noreply.github.com>
2025-02-19 08:59:06 +0000

    feat: Gameboard class complete with tests.

--- src/main.js
+++ src/main.js
@@ -17,9 +17,46 @@ class Ship{
     }
 }
 
-class GameBoard{
 
+
+
+class GameBoard{
+    constructor(){
+        this.gameMap = [];
+        this.gameMap.push(new verticeCord('empty', [], []));
+    }
+
+    placeShip(length, x ,y){
+        this.gameMap.push(verticeCord(new Ship(length),x , y));
+    }
+
+    receiveAttack(xC, yC){
+        this.gameMap.forEach((vertice)=> {
+            if(vertice.type instanceof Ship && vertice.x.includes(xC) && vertice.y.includes(yC)){
+                
+                vertice.type.hit();
+                return;
+            }
+            this.createEmpty(xC, yC);
+        })
+    }
+
+    createEmpty(xC, yC){
+        this.gameMap[0].x.push(xC);
+        this.gameMap[0].y.push(yC);
+    }
+
+    checkShips(){
+        return this.gameMap
+        .filter((vertice) => vertice.type instanceof Ship)
+        .every((ship) => ship.type.sunk === true);
+    }
+}   
+
+function verticeCord(type,x, y){
+    return { type, x: Array.isArray(x) ? x : [x], y: Array.isArray(y) ? y : [y] };
 
 }
 
-module.exports={Ship}
+
+module.exports={Ship, GameBoard, verticeCord}
--- src/main.test.js
+++ src/main.test.js
@@ -17,5 +17,57 @@ describe("Ship Class", () => {
 
         expect(testShip.sunk).toBe(true);
     });
+});
 
-})
+describe("Vertice constructor",() => {
+
+    test("Test vertice constructor with empty", () => {
+        let vertice = ship.verticeCord("empty", 1, 1);
+        expect(vertice).toEqual({type: "empty", x: [1], y: [1]});
+    })
+
+    test("Test vertice constructor with ship.", () => {
+        let vertice = new ship.Ship(4, [0,1,2,3,4], [0,0,0,0]);
+        console.log(vertice);
+        expect(vertice).toEqual({hits: 0, length: 4, sunk: false,});
+    })
+});
+
+
+describe("Gameboard Class", () => {
+    let gameboard;
+
+    beforeEach(()=> {
+        gameboard = new ship.GameBoard();
+        gameboard.placeShip(4, [0,1,2,3], [0,0,0,0]);
+    })
+
+    test("If Gameboard placeShip() pushes to array.", () => {
+        
+        expect(gameboard.gameMap[1].type.hits).toBe(0);
+    });
+
+    test("If Gameboard receiveAttack() on ship.", () => {
+        gameboard.receiveAttack(1,0);
+        expect(gameboard.gameMap[1].type.hits).toBe(1);
+    });
+
+    test("If Gameboard receiveAttack() on empty land.", () => {
+        gameboard.receiveAttack(1,1);
+        expect(gameboard.gameMap[1].type.hits).toBe(0);
+        expect(gameboard.gameMap[0].type).toBe("empty");
+    });
+
+    test("If Gameboard checkShips() works on ships.", () => {
+        expect(gameboard.checkShips()).toBe(false);
+    })
+
+    test("If Gameboard checkShips() works on ships.", () => {
+        gameboard.receiveAttack(0,0);
+        gameboard.receiveAttack(1,0);
+        gameboard.receiveAttack(2,0);
+        gameboard.receiveAttack(3,0);
+        expect(gameboard.checkShips()).toBe(true);
+    })
+});
+
