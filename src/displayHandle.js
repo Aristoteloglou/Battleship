764ecb7822be029f076083f8d8c45c3c1b1f46fb
Aristoteloglou <60239870+Aristoteloglou@users.noreply.github.com>
2025-02-25 21:28:12 +0000

    feat: Add ship tile to html.

--- src/displayHandle.js
+++ src/displayHandle.js
@@ -14,3 +14,36 @@ p2.gameboard.placeShip(3, [0, 1, 2], [1, 1, 1]);
 p2.gameboard.placeShip(4, [0, 1, 2, 3], [2, 2, 2, 2]);
 p2.gameboard.placeShip(2, [0, 1], [3, 3]);
 p2.gameboard.placeShip(5, [0, 1, 2, 3, 4], [4, 4, 4, 4, 4]);
+
+
+
+class renderGame{
+
+    static gameRender(){
+        let x = 0;
+        let y = 0;
+        while(x < 10 ){
+            while(y < 10){
+                if(x === p1.gameboard[0].x && y === p1.gameboard[0].x){
+
+                }
+            }
+        }
+    }
+
+
+
+
+
+}
+
+
+
+
+
+
+/* Figurre out a way ti establush the rendering of items in database. 
+Issue is there's no "empty" item tp save space, so how will the rendering happen, in way everything is structured appropriately.
+Tinkn of how to append things to gameboard appropriately with making it make sense. Think maybe all the data should be in th database.
+And UI is literally interaction to daabase. Maybe dataset on div tags could work, but idk.
+*/
